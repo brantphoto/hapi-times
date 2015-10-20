76d6862b16e7f7aa2b56e87172578caa54cb8fb2
brantphoto <brant.barger@gmail.com>
2015-10-19 17:00:54 -0700

    fixed test

--- test/unit.js
+++ test/unit.js
@@ -15,7 +15,7 @@ lab.experiment("Users", function() {
         var result = response.result;
 
         expect(response.statusCode).to.equal(200);
-        expect(result).to.be.array();
+        expect(result).to.be.string();
 
         done();
 
